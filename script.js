3cfadd5c6a0eaedf2926efcff74f634c1ff89e7e
warrior-coder <gudkou2003@gmail.com>
2021-03-29 18:51:33 +0300

    4 sides interaction

--- script.js
+++ script.js
@@ -17,67 +17,56 @@ const _COVERS = [
 ];
 
 const _CUBES = [
-    { translates: [0, 0, 0], rotates: [0, 0, 0,], colors: ['-','blue','orange','-','white','-'], origins: [100, 100, 0] },
-    { translates: [100, 0, 0], rotates: [0, 0, 0,], colors: ['-','blue','-','red','white','-'], origins: [0, 100, 0] },
-    { translates: [0, 0, 100], rotates: [0, 0, 0,], colors: ['lime','-','orange','-','white','-'], origins: [100, 100, -100] },
-    { translates: [100, 0, 100], rotates: [0, 0, 0,], colors: ['lime','-','-','red','white','-'], origins: [0, 100, -100] },
+    { translates: [0, 0, 0], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['-','blue','orange','-','white','-'], origins: [100, 100, 0] },
+    { translates: [100, 0, 0], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['-','blue','-','red','white','-'], origins: [0, 100, 0] },
+    { translates: [0, 0, 100], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['lime','-','orange','-','white','-'], origins: [100, 100, -100] },
+    { translates: [100, 0, 100], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['lime','-','-','red','white','-'], origins: [0, 100, -100] },
     
-    { translates: [0, 100, 0], rotates: [0, 0, 0,], colors: ['-','blue','orange','-','-','yellow'], origins: [100, 0, 0] },
-    { translates: [100, 100, 0], rotates: [0, 0, 0,], colors: ['-','blue','-','red','-','yellow'], origins: [0, 0, 0] },
-    { translates: [0, 100, 100], rotates: [0, 0, 0,], colors: ['lime','-','orange','-','-','yellow'], origins: [100, 0, -100] },
-    { translates: [100, 100, 100], rotates: [0, 0, 0,], colors: ['lime','-','-','red','-','yellow'], origins: [0, 0, -100] },
-
-    // { tranX: 100, tranY: 0, tranZ: -50, rotX: 0, rotY: 0, rotZ: 0 },
-    // { tranX: 0, tranY: 0, tranZ: 50, rotX: 0, rotY: 0, rotZ: 0 },
-    // { tranX: 100, tranY: 100, tranZ: 50, rotX: 0, rotY: 0, rotZ: 0 },
+    { translates: [0, 100, 0], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['-','blue','orange','-','-','yellow'], origins: [100, 0, 0] },
+    { translates: [100, 100, 0], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['-','blue','-','red','-','yellow'], origins: [0, 0, 0] },
+    { translates: [0, 100, 100], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['lime','-','orange','-','-','yellow'], origins: [100, 0, -100] },
+    { translates: [100, 100, 100], rotate3d: { vector: '0,0,0', deg: 0 }, colors: ['lime','-','-','red','-','yellow'], origins: [0, 0, -100] },
 ];
 
-const AXIS_X = 0, AXIS_Y = 1, AXIS_Z = 2, _CUBESIZE = 2, _MATRIX = [
-    [
-        [ 0, 1 ],
-        [ 2, 3 ]
-    ],
-    [
-        [ 4, 5 ],
-        [ 6, 7 ]
-    ]
-];
-
-var ISIDE_VOID = false, ISIDE_CUBE = false, MOUSE_DOWN = false;
+var ISIDE_VOID = false, ISIDE_CUBE = false, MOUSE_DOWN = false, IS_ROTATING = false;
 var mX0, mY0, cubeRotX = 0, cubeRotY = 0;
-var dX, dY, delay, e0, rotX, rotY, rotZ, i0, j0, k0, side_i, side_j, dR, dir;
+var dX, dY, delay, e0, rotX, rotY, rotZ, i0, j0, k0, side_i, side_j, constDimension, vector, dir;
 
 const VUE1 = new Vue({
     el: '#VUE1',
     data: {
         sides: _SIDES,
         cubes: _CUBES,
-        rotatingCubes: [1,2,3],
+        rotatingCubes: [],
+        rotatedCovers: [],
         covers: _COVERS,
         cubeRotX: 0,
         cubeRotY: 0,
         cubeRotZ: 0,
-        matrix: _MATRIX,
+        cubeTransition: 1,
     },
     methods: {
         voidMouseDown(e)
         {
-            if (e.target == document.getElementById('VUE1'))
-            {
-                ISIDE_VOID = true;
-                ISIDE_CUBE = false;
-            }
-            else if (e.target.className.indexOf('cover') != -1)
+            side_i = -1; side_j = -1;
+            if (e.target.className.indexOf('cover') != -1 && IS_ROTATING == false)
             {
                 ISIDE_VOID = false;
                 ISIDE_CUBE = true;
+
                 side_i = Math.trunc(e.offsetY / 100);
                 side_j = Math.trunc(e.offsetX / 100);
+                console.log(side_i, side_j);
 
                 dX = 0;
                 dY = 0;
                 delay = 0;
                 e0 = e;
+            }
+            else if (e.target == document.getElementById('VUE1'))
+            {
+                ISIDE_VOID = true;
+                ISIDE_CUBE = false;
             }
             
             MOUSE_DOWN = true;
@@ -88,7 +77,6 @@ const VUE1 = new Vue({
         voidMouseUp(e)
         {
             MOUSE_DOWN = false;
-            // console.log('TOTAL CUBE ROTATE:',rotX, rotY, rotZ);
         },
         voidMouseMove(e)
         {
@@ -110,68 +98,229 @@ const VUE1 = new Vue({
             }
 
             //-+-+-+-+-+-+-+-+-+-+-CUBE-+-+-+-+-+-+-+-+-+-+-
-            if (ISIDE_CUBE && MOUSE_DOWN)
+            else if (ISIDE_CUBE && MOUSE_DOWN && IS_ROTATING == false && side_j > -1)
             {
                 dX += (e.pageX - mX0);
                 dY += (e.pageY - mY0);
                 delay++;
                 if (delay > 10)
                 {
-                    console.log(dX,dY);
-                    
-                    let Axis, constDimension
+                    IS_ROTATING = true;
+                    this.cubeTransition  = .5;
+
                     switch (e0.target.innerText) 
                     {
                         //----------TARGET-FRONT----------
                         case 'front':
-                            console.log('---TARGET-FRONT---');
                             if (Math.abs(dX) > Math.abs(dY))
                             {
-                                dR = 'x';
                                 dir = (dX > 0) ? 1 : -1; 
+                                vector = '0,1,0';
                                 constDimension = 'k';
-                                Axis = AXIS_Y;
                             }
                             else
                             {
-                                dR = 'y';
                                 dir = (dY > 0) ? -1 : 1; 
                                 constDimension = 'j';
-                                Axis = AXIS_X;
+                                vector = '1,0,0';
                             }
-
-                            this.matrix = this.RotateMatrix3D(this.matrix, _CUBESIZE, constDimension, 1, side_j, side_i, -dir);
-                            this.RotateCube3D(Axis, dir);
-
+                            this.matrix = this.RotateMatrix3D(constDimension, 1, side_j, side_i, -dir);
+                            this.RotateCube3D(vector, dir);
                         break;
-                    
+                        case 'back':
+                            if (Math.abs(dX) > Math.abs(dY))
+                            {
+                                dir = (dX > 0) ? 1 : -1; 
+                                vector = '0,1,0';
+                                constDimension = 'k';
+                            }
+                            else
+                            {
+                                dir = (dY > 0) ? 1 : -1; 
+                                constDimension = 'j';
+                                vector = '1,0,0';
+                            }
+                            this.matrix = this.RotateMatrix3D(constDimension, 1, 1-side_j, side_i, -dir);
+                            this.RotateCube3D(vector, dir);
+                        break;
+                        case 'left':
+                            if (Math.abs(dX) > Math.abs(dY))
+                            {
+                                dir = (dX > 0) ? 1 : -1; 
+                                vector = '0,1,0';
+                                constDimension = 'k';
+                            }
+                            else
+                            {
+                                dir = (dY > 0) ? -1 : 1; 
+                                constDimension = 'i';
+                                vector = '0,0,1';
+                            }
+                            this.matrix = this.RotateMatrix3D(constDimension, side_j, 1, side_i, -dir);
+                            this.RotateCube3D(vector, dir);
+                        break;
+                        case 'right':
+                            if (Math.abs(dX) > Math.abs(dY))
+                            {
+                                dir = (dX > 0) ? 1 : -1; 
+                                vector = '0,1,0';
+                                constDimension = 'k';
+                            }
+                            else
+                            {
+                                dir = (dY > 0) ? 1 : -1; 
+                                constDimension = 'i';
+                                vector = '0,0,1';
+                            }
+                            this.matrix = this.RotateMatrix3D(constDimension, 1-side_j, 1, side_i, -dir);
+                            this.RotateCube3D(vector, dir);
+                        break;
+                        // case 'top':
+                        //     if (Math.abs(dX) > Math.abs(dY))
+                        //     {
+                        //         dir = (dY > 0) ? 1 : -1; 
+                        //         constDimension = 'i';
+                        //         vector = '0,0,1';
+                        //     }
+                        //     else
+                        //     {
+                                
+                        //         dir = (dX > 0) ? 1 : -1; 
+                        //         vector = '1,0,0';
+                        //         constDimension = 'j';
+                        //     }
+                        //     this.matrix = this.RotateMatrix3D(constDimension, 1-side_j, 1-side_i, 1, -dir);
+                        //     this.RotateCube3D(vector, dir);
+                        // break;
                         default:
                             console.log('OTHER');
                         break;
                     }
-                 
-                    MOUSE_DOWN = false;
-                    delay = 0;
-                    dX = 0;
-                    dY = 0;
+                    
+                    
+                    setTimeout(() => {
+                        this.cubeTransition = 0;
+
+                        for (let i = 0; i < this.rotatingCubes.length; i++)
+                        {
+                            this.cubes[this.rotatingCubes[i]].rotate3d.vector = '0,0,0';
+                            this.cubes[this.rotatingCubes[i]].rotate3d.deg = 0
+                            this.cubes[this.rotatingCubes[i]].colors = this.UpdateCubeCover(this.rotatedCovers[i], constDimension, dir); 
+                        }
+
+                        IS_ROTATING = false;
+                        MOUSE_DOWN = false;
+                        delay = 0;
+                        dX = 0;
+                        dY = 0;
+                    },400);
+                   
                 }
                 
                 
             }
         },
-        RotateCube3D(Axis, direction)
+        UpdateCubeCover(cover, constDimension, direction)
+        {
+            let updatedCover = [];
+            //-+-+-+-+-+-+-+-+-+-+-I-+-+-+-+-+-+-+-+-+-+-
+            if (constDimension == 'i')
+            {
+                if (direction == 1)
+                {
+                    updatedCover[0] = cover[0]; // CONST
+                    updatedCover[1] = cover[1]; // CONST
+                    updatedCover[2] = cover[5];
+                    updatedCover[3] = cover[4];
+                    updatedCover[4] = cover[2];
+                    updatedCover[5] = cover[3]; 
+                }
+                else 
+                {
+                    updatedCover[0] = cover[0]; // CONST
+                    updatedCover[1] = cover[1]; // CONST
+                    updatedCover[2] = cover[4];
+                    updatedCover[3] = cover[5];
+                    updatedCover[4] = cover[3];
+                    updatedCover[5] = cover[2]; 
+                }
+            }
+            //-+-+-+-+-+-+-+-+-+-+-J-+-+-+-+-+-+-+-+-+-+-
+            else if (constDimension == 'j')
+            {
+                if (direction == 1)
+                {
+                    updatedCover[0] = cover[5]; 
+                    updatedCover[1] = cover[4]; 
+                    updatedCover[2] = cover[2]; // CONST
+                    updatedCover[3] = cover[3]; // CONST
+                    updatedCover[4] = cover[0];
+                    updatedCover[5] = cover[1]; 
+                }
+                else 
+                {
+                    updatedCover[0] = cover[4]; 
+                    updatedCover[1] = cover[5]; 
+                    updatedCover[2] = cover[2]; // CONST
+                    updatedCover[3] = cover[3]; // CONST
+                    updatedCover[4] = cover[1];
+                    updatedCover[5] = cover[0]; 
+                }
+                
+            }
+            //-+-+-+-+-+-+-+-+-+-+-K-+-+-+-+-+-+-+-+-+-+-
+            else if (constDimension == 'k')
+            {
+                if (direction == 1)
+                {
+                    updatedCover[0] = cover[2]; 
+                    updatedCover[1] = cover[3]; 
+                    updatedCover[2] = cover[1]; 
+                    updatedCover[3] = cover[0];
+                    updatedCover[4] = cover[4]; // CONST
+                    updatedCover[5] = cover[5]; // CONST
+                }
+                else 
+                {
+                    updatedCover[0] = cover[3]; 
+                    updatedCover[1] = cover[2]; 
+                    updatedCover[2] = cover[0];
+                    updatedCover[3] = cover[1];
+                    updatedCover[4] = cover[4]; // CONST
+                    updatedCover[5] = cover[5]; // CONST
+                }
+            }
+
+            return updatedCover;
+        },
+        RotateCube3D(vector, direction)
         {
             for (let index of this.rotatingCubes)
             {
-                let deg = this.cubes[index].rotates[Axis];
-                deg += direction * 90;
-                this.$set(this.cubes[index].rotates, Axis, deg);
+                let deg = direction * 90;
+                this.cubes[index].rotate3d.vector = vector;
+                this.cubes[index].rotate3d.deg = deg;
             }
         },
-        RotateMatrix3D(matrix3d, size, constDimension, I0, J0, K0, direction)
+        // RotateCubeCoverc(cubeCover, size, )
+        // {
+            
+        // }
+        RotateMatrix3D(constDimension, I0, J0, K0, direction)
         {
+            let size = 2, matrix3d = [
+                [
+                    [ 0, 1 ],
+                    [ 2, 3 ]
+                ],
+                [
+                    [ 4, 5 ],
+                    [ 6, 7 ]
+                ]
+            ];
             let temp;
             this.rotatingCubes.splice(0);
+            this.rotatedCovers.splice(0);
             //-+-+-+-+-+-+-+-+-+-+-I-+-+-+-+-+-+-+-+-+-+-
             if (constDimension == 'i')
             {
@@ -191,16 +340,6 @@ const VUE1 = new Vue({
                 // MIRRORATE
                 if (direction == 1)
                 {
-                    for (let k = 0; k < size; k++)
-                        for (let j = 0; j < Math.trunc(size/2); j++)
-                        {
-                            temp = matrix3d[k][I0][j];
-                            matrix3d[k][I0][j] = matrix3d[k][I0][size-j-1];
-                            matrix3d[k][I0][size-j-1] = temp;
-                        }
-                }
-                else
-                {
                     for (let j = 0; j < size; j++)
                         for (let k = 0; k < Math.trunc(size/2); k++)
                         {
@@ -209,6 +348,22 @@ const VUE1 = new Vue({
                             matrix3d[size-k-1][I0][j] = temp;
                         }
                 }
+                else
+                {
+                    
+                    for (let k = 0; k < size; k++)
+                        for (let j = 0; j < Math.trunc(size/2); j++)
+                        {
+                            temp = matrix3d[k][I0][j];
+                            matrix3d[k][I0][j] = matrix3d[k][I0][size-j-1];
+                            matrix3d[k][I0][size-j-1] = temp;
+                        }
+                }
+
+                 // COLLECT CUBES 2
+                 for (let k = 0; k < size; k++)
+                    for (let j = 0; j < size; j++)
+                        this.rotatedCovers.push(this.cubes[matrix3d[k][I0][j]].colors);
             }
 
             //-+-+-+-+-+-+-+-+-+-+-J-+-+-+-+-+-+-+-+-+-+-
@@ -248,6 +403,11 @@ const VUE1 = new Vue({
                             matrix3d[size-k-1][i][J0] = temp;
                         }
                 }
+
+                // COLLECT CUBES 2
+                for (let k = 0; k < size; k++)
+                    for (let i = 0; i < size; i++)
+                        this.rotatedCovers.push(this.cubes[matrix3d[k][i][J0]].colors);
             }
 
             //-+-+-+-+-+-+-+-+-+-+-K-+-+-+-+-+-+-+-+-+-+-
@@ -287,14 +447,18 @@ const VUE1 = new Vue({
                             matrix3d[K0][size-i-1][j] = temp;
                         }
                 }
+
+                // COLLECT CUBES 2
+                for (let i = 0; i < size; i++)
+                    for (let j = 0; j < size; j++)
+                        this.rotatedCovers.push(this.cubes[matrix3d[K0][i][j]].colors);
+
             }
             return matrix3d;
         }
     },
     mounted()
     {
-
-       
 
         // console.log(this.matrix);
         // this.$set(this.cubes[3].rotates, 0, 15);
